8d5500e8e05a09d27e4362be58faa120b5e46953
Mario Pescarmona <mpescarmona@gmail.com>
2019-10-30 01:09:18 -0300

    First version of custom exception handler

---
+++ src/main/java/com/mpescarmona/demorestfulapi/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.mpescarmona.demorestfulapi.configuration;
+
+import com.mpescarmona.demorestfulapi.domain.response.ErrorMessage;
+import com.mpescarmona.demorestfulapi.exception.UserAlreadyRegisteredException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyRegisteredException.class)
+    public ResponseEntity<ErrorMessage> springHandleUserAlreadyRegistered(Exception ex) {
+        ErrorMessage errors = new ErrorMessage();
+        errors.setError(ex.getMessage());
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    // @Validate For Validating Path Variables and Request Parameters
+    @ExceptionHandler(ConstraintViolationException.class)
+    public void constraintViolationException(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value());
+    }
+
+    // error handle for @Valid
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        //Get all errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
+    }
+}
