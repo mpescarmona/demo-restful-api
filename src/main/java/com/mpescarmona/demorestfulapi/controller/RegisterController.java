486f9f177973127215ffd9168765d261c1d6a384
Mario Pescarmona <mpescarmona@gmail.com>
2019-10-30 01:10:17 -0300

    Update Register controller

--- src/main/java/com/mpescarmona/demorestfulapi/controller/RegisterController.java
+++ src/main/java/com/mpescarmona/demorestfulapi/controller/RegisterController.java
@@ -1,24 +1,30 @@
 package com.mpescarmona.demorestfulapi.controller;
 
 import com.mpescarmona.demorestfulapi.domain.User;
-import com.mpescarmona.demorestfulapi.domain.response.ErrorMessage;
+import com.mpescarmona.demorestfulapi.exception.UserAlreadyRegisteredException;
+import com.mpescarmona.demorestfulapi.exception.UserNotFoundException;
 import com.mpescarmona.demorestfulapi.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequestMapping("/demo-api")
+@Validated
+@RequestMapping("/demo-api/user")
 public class RegisterController {
 
     @Autowired
@@ -27,17 +33,29 @@ public class RegisterController {
     @PostMapping(path = "/register",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> registerNewUser(@RequestBody @Valid User user) {
+    public ResponseEntity<?> registerNewUser(@RequestBody
+                                             @Valid
+                                                     User user) {
         log.info("action=registerNewUser");
         Optional<User> optionalUser = userService.findUserByEmail(user.getEmail());
+
         if (optionalUser.isPresent()) {
-            log.info("action=registerNewUser, The provided email is already registered={}", user.getEmail());
-            ErrorMessage emailAlredyExistsMessage = ErrorMessage.builder()
-                    .message("The provided email '" + user.getEmail() + "' is already registered")
-                    .build();
-            return new ResponseEntity<>(emailAlredyExistsMessage, HttpStatus.PRECONDITION_FAILED);
+            throw new UserAlreadyRegisteredException(user.getEmail());
         }
+
         User newUser = userService.registerUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{email}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> findUserByEmail(@PathVariable
+                                             @Email(message = "Email should be valid")
+                                                     String email) {
+        log.info("action=findUserByEmail");
+        User user = userService.findUserByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException(email));
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
